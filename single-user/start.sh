8657a6f53a540507de81e3d28f7fffcde7805eb2
yjt <961549745@qq.com>
2018-09-05 18:45:57 +0800

    最终提交

--- single-user/start.sh
+++ single-user/start.sh
@@ -1,9 +1,9 @@
 echo "config ss server ..."
 server=`python -m site --user-site`/shadowsocks/server.py
-echo "port: "
+echo -n "port: "
 read port
-echo "method: (aes-256-cfb)" 
-read method
-echo "password: "
+echo -n "password: "
 read psw
-sudo python $server -p $port -m $method -k $psw --log-file sslog.log -d start 
+echo "method: (aes-256-cfb | aes-256-cfb1 | aes-256-cfb8 | aes-256-ctr | rc4-md5)"
+read method
+sudo python $server -p $port -m $method -k $psw --log-file sslog.log -d start
