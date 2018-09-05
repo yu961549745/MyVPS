5ee57ed15df25a86897542619a812e2c87ebbd8a
yjt <961549745@qq.com>
2018-09-05 19:43:04 +0800

    提交

--- single-user/README.md
+++ single-user/README.md
@@ -1,4 +1,7 @@
 # 仅 shadowsocks 后端 pip 直接安装, 适合少量用户直接使用
 ```bash
-wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/install.sh install.sh && sudo sh install.sh
+mkdir ss
+cd ss
+wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/install.sh 
+sudo sh install.sh
 ```
--- single-user/install.sh
+++ single-user/install.sh
@@ -1,7 +1,7 @@
 apt clean all && apt autoremove -y && apt update --fix-missing && apt upgrade -y && apt dist-upgrade -y
 apt install python-pip
 pip install shadowsocks-py
-wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/start.sh   start.sh
-wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/stop.sh    stop.sh 
-wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/showlog.sh showlog.sh 
+wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/start.sh  
+wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/stop.sh    
+wget https://raw.githubusercontent.com/yu961549745/MyVPS/master/single-user/showlog.sh 
 sh start.sh 
