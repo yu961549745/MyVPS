fce232c7e29e654572d6cc88100a7c1a7d93e9df
yjt <961549745@qq.com>
2018-09-05 19:31:39 +0800

    最后提交

--- single-user/start.sh
+++ single-user/start.sh
@@ -1,9 +1,24 @@
-echo "config ss server ..."
 server=`python -m site --user-site`/shadowsocks/server.py
+config_file=".ss.config.json"
+if [ -f "$config_file" ]; then
+    sudo python $server -c $config_file --log-file sslog.log -d start
+    exit
+else
+    echo "config ss server ..."
+fi
 echo -n "port: "
 read port
 echo -n "password: "
 read psw
 echo "method: (aes-256-cfb | aes-256-cfb1 | aes-256-cfb8 | aes-256-ctr | rc4-md5)"
 read method
+echo "{
+    \"server_port\": $port,
+    \"password\": \"$psw\",
+    \"method\": \"$method\"
+}" > $config_file
 sudo python $server -p $port -m $method -k $psw --log-file sslog.log -d start
+echo "show server info by "
+echo "              cat $config_file"
+echo "reset server by "
+echo "              rm $config_file && sh start.sh"
